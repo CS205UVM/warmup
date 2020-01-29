57a0e36f8ec4551d47ae4220a193cbcdd19f7b0d
kefah123 <kefah@uvm.edu>
2020-01-29 08:54:11 -0500

    add

---
+++ Parser.py
@@ -0,0 +1,14 @@
+load data
+help
+quit
+select all songs
+select all artists
+select songs and their genres
+select songs and their track lengths
+individual artist and their song
+song and its popularity
+select songs where rank is < some value
+select songs where length is < some value
+select songs where length is > some value
+select songs where popularity is < some value
+select songs where popularity is > some value
