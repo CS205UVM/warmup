c60d29caff7312d523d8d45bda1cf48ffa4ff6dd
Jordan D <54602007+jrdeso@users.noreply.github.com>
2020-01-29 16:41:01 -0500

    started creating initial search commands for songs/artists

--- Parser.py
+++ Parser.py
@@ -1,4 +1,5 @@
 """
+IDEA LIST FOR POSSIBLE COMMANDS
 load data
 help
 quit
@@ -15,16 +16,87 @@ select songs where popularity is < some value
 select songs where popularity is > some value
 """
 
+# keep program running and reentering the CLI, in the future program will run
+# until quit command is issued.
+def main():
+    print("Welcome to Song/Artist Parser")  # Greeting can be changed later
+    while True:
+        # Get command from user
+        request = input("")
+
+        # Conditionals corresponding to words entered
+        # To view help options
+        if request == "help":
+            help()
+
+        # to quit program
+        if request == "quit":
+            quit()
+
+        # To view the top songs in the database
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view the top artists in the database
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view a range of top songs
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view a range of top artists
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view all songs of a specific genre
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view all songs from a particular artist
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view songs within a particular length
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view the top songs in the database
+        if request == "":
+            pass  # need QueryFunctions method
+
+        # To view the popularity rank of a particular artist
+        if request == "":
+            pass  # need QueryFunctions method
+"""
+one of the commands should be load data, which will create the database 
+and the schema and read data from your csv ﬁles into the tables; if the 
+database already exists, then the command will overwrite the existing database
+"""
 def loadData():
+    pass
 
-
+"""
+one of the commands should be help, which will print out help text about the commands
+"""
 def help():
+    print("Help Section - Note that you must include quotations around input where directed. ")  # Can change message later
+    print("To view the top songs in the database, type 'show all songs'")
+    print("To view the top artists in the database, type 'show all artists'")
+    print("To view a range of top songs, type 'songs range \"LOWVAL\" to \"HIGHVAL\"'")
+    print("To view a range of top artists, type 'artists range \"LOWVAL\" to \"HIGHVAL\"'")
+    print("To view all songs of a specific genre, type 'songs genre \"SAMPLEGENRE\"'")
+    print("To view all songs from a particular artist, type 'artist songs \"SAMPLEARTIST\"'")
+    print("To view songs within a particular length, type 'songs length \"LOWVAL\" to \"HIGHVAL\"'") #TODO: convert to seconds/minutes? (how does user enter this)
+    print("To view the popularity rank of a particular artist, type 'artist popularity \"ARTIST NAME\"'")
+    #TODO: More commands...?
 
 
 def quit():
-
+    exit(0)
 
 def allSongs():
+    pass
 
 
-def
+main()
