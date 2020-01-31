0f231aa3933f43d242c220791023cc0b87e3fa06
Jade <jade.oconnor@uvm.edu>
2020-01-31 08:31:41 -0500

    queury functions

---
+++ Queryfunctions.py
@@ -0,0 +1,59 @@
+# if the user wants a list of all songs
+def allSongs():
+
+    return
+
+# if the user wants a list of all artists
+def allArtists():
+
+    return
+
+# if the user requests an artist and wants their song
+def artistAndSong():
+
+    return
+
+# if the user requests a song and wants the artist
+def songAndArtist():
+
+    return
+
+# if a user requests a song and wants the popularity
+def songAndRank():
+
+    return
+
+# if a user requests a song and wants its genre
+def songAndGenre():
+
+    return
+
+# if a user requests a genre and wants its songs
+def genreAndSong():
+
+    return
+
+#if a user requests a song and its length
+def songAndLength():
+
+    return
+
+# if a user wants songs less than a certain popularity
+def songLessRank(rank):
+
+    return
+
+#if a user wants osngs more than a certain popularity
+def songMoreRank(rank):
+
+    return
+
+#if a user wants songs less than a certain length
+def songLessLength(length):
+
+    return
+
+#if a user wants osngs more than a certain length
+def songMoreLength(length):
+
+    return
