2981bae9bd1e3106574bc05ad8299dc9a7744526
mattzahar <mzahar@uvm.edu>
2020-02-18 18:11:40 -0500

    Finilized the csv reader

--- Parser.py
+++ Parser.py
@@ -16,8 +16,7 @@ select songs where popularity is < some value
 select songs where popularity is > some value
 """
 import Queryfunctions
-import re
-import sqlite3
+import sqlite3, csv, os
 from sqlite3 import Error
 
 
@@ -29,7 +28,7 @@ def main():
     print("Hint: type help to get a list of commands")
 
     Query = Queryfunctions
-    loadData()
+    load()
 
     while True:
         # Get command from user
@@ -455,118 +454,34 @@ one of the commands should be load data, which will create the database
 and the schema and read data from your csv ﬁles into the tables; if the 
 database already exists, then the command will overwrite the existing database
 """
-def loadData():
-    database = "warmup.db"
 
-    sql_create_artist_table = """ CREATE TABLE artists (
-                                        `artist_name` VARCHAR(16),
-                                        `genre` VARCHAR(16),
-                                        `popularity` INT,
-                                        `song_name` VARCHAR(21)
-                                        ); """
 
-    sql_create_song_table = """CREATE TABLE songs (
-                                    `rank` INT,
-                                    `track_name` VARCHAR(21),
-                                    `length_sec` INT,
-                                    `artist_name` VARCHAR(16)
-                                    );"""
+def load():
+    # referenced https://stackoverflow.com/questions/2887878/importing-a-csv-file-into-a-sqlite3-database-table-using-python
 
-    sqlite_insert_song_query = """
-        INSERT INTO songs VALUES
-        (1,'China',302,'Anuel AA'),
-        (2,'boyfriend',186,'Ariana Grande'),
-        (3,'Ransom',131,'Lil Tecca'),
-        (4,'How Do You Sleep',202,'Sam Smith'),
-        (5,'Callaita',251,'Bad Bunny'),
-        (6,'Loco Contigo',185,'DJ Snake'),
-        (7,'Someone You Loved',182,'Lewis Capaldi'),
-        (8,'Money In The Grave',205,'Drake'),
-        (9,'No Guidance',261,'Chris Brown'),
-        (10,'Sunflower',158,'Post Malone'),
-        (11,'Lalala',161,'Y2K'),
-        (12,'Truth Hurts',173,'Lizzo'),
-        (13,'Piece Of Your Heart',153,'MEDUZA'),
-        (14,'Panini',115,'Lil Nas X'),
-        (15,'No Me Conoce',309,'Jhay Cortez'),
-        (16,'Soltera',266,'Lunay'),
-        (17,'bad guy',195,'Billie Eilish'),
-        (18,'If I Can''t Have You',191,'Shawn Mendes'),
-        (19,'Dance Monkey',210,'Tones and I'),
-        (20,'It''s You',213,'Ali Gatie'),
-        (21,'Con Calma',193,'Daddy Yankee'),
-        (22,'QUE PRETENDES',222,'J Balvin'),
-        (23,'The London',200,'Young Thug'),
-        (24,'Never Really Over',224,'Katy Perry'),
-        (25,'Summer Days',164,'Martin Garrix'),
-        (26,'Otro Trago',226,'Sech'),
-        (27,'Sucker',181,'Jonas Brothers'),
-        (28,'fuck, i''m lonely',199,'Lauv'),
-        (29,'Higher Love',228,'Kygo'),
-        (30,'You Need To Calm Down',171,'Taylor Swift'),
-        (31,'Shallow',216,'Lady Gaga'),
-        (32,'Talk',198,'Khalid'),
-        (33,'Con Altura',162,'ROSALÍA'),
-        (34,'Te Robaré',202,'Nicky Jam'),
-        (35,'Happier',214,'Marshmello'),
-        (36,'Call You Mine',218,'The Chainsmokers'),
-        (37,'Cross Me',206,'Ed Sheeran');
+    if os.path.exists("warmup.db"):
+        os.remove("warmup.db")
 
-        """
+    con = sqlite3.connect("warmup.db")
+    cur = con.cursor()
 
-    sqlite_insert_artist_query = """
-        INSERT INTO artists VALUES
-        ('Ali Gatie','canadian hip hop',89,'It''s You'),
-        ('Anuel AA','reggaeton flow',92,'China'),
-        ('Ariana Grande','dance pop',89,'7 rings'),
-        ('Bad Bunny','reggaeton',93,'Callaita'),
-        ('Billie Eilish','electropop',89,'bad guy'),
-        ('Chris Brown','dance pop',82,'No Guidance'),
-        ('Daddy Yankee','latin',91,'Con Calma'),
-        ('DJ Snake','dance pop',86,'Loco Contigo'),
-        ('Drake','canadian hip hop',92,'Money In The Grave '),
-        ('Ed Sheeran','pop',82,'Cross Me'),
-        ('J Balvin','latin',89,'QUE PRETENDES'),
-        ('Jhay Cortez','reggaeton flow',83,'No Me Conoce'),
-        ('Jonas Brothers','boy band',80,'Sucker'),
-        ('Katy Perry','dance pop',89,'Never Really Over'),
-        ('Khalid','pop',84,'Talk'),
-        ('Kygo','edm',88,'Higher Love'),
-        ('Lady Gaga','dance pop',87,'Shallow'),
-        ('Lauv','dance pop',78,'fuck, i''m lonely'),
-        ('Lewis Capaldi','pop',88,'Someone You Loved'),
-        ('Lil Nas X','country rap',91,'Panini'),
-        ('Lil Tecca','trap music',92,'Ransom'),
-        ('Lizzo','escape room',91,'Truth Hurts'),
-        ('Lunay','latin',91,'Soltera'),
-        ('Marshmello','brostep',88,'Happier'),
-        ('Martin Garrix','big room',89,'Summer Days'),
-        ('MEDUZA','pop house',91,'Piece Of Your Heart'),
-        ('Nicky Jam','latin',88,'Te Robaré'),
-        ('Post Malone','dfw rap',91,'Sunflower'),
-        ('ROSALÍA','r&b en espanol',88,'Con Altura'),
-        ('Sam Smith','pop',90,'How Do You Sleep'),
-        ('Sech','panamanian pop',91,'Otro Trago'),
-        ('Shawn Mendes','canadian pop',70,'If I Can''t Have You'),
-        ('Taylor Swift','dance pop',90,'You Need To Calm Down'),
-        ('The Chainsmokers','edm',88,'Call You Mine'),
-        ('Tones and I','australian pop',83,'Dance Monkey'),
-        ('Young Thug','atl hip hop',89,'The London'),
-        ('Y2K','canadian hip hop',88,'Lalala');"""
+    cur.execute("CREATE TABLE artists (artist_name, genre , popularity, song_name);")  # use your column names here
+    with open('Artist-Table.csv', 'rt') as fin:  # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i['artist_name'], i['genre'], i['popularity'], i['song_name']) for i in dr]
+    cur.executemany("INSERT INTO artists (artist_name, genre, popularity, song_name) VALUES (?, ?, ?, ?);", to_db)
 
-    # create a database connection
-    conn = create_connection(database)
+    cur.execute("CREATE TABLE songs(rank, track_name, length, artist_name);")  # use your column names here
+    with open('Song-Table.csv', 'rt') as fin:  # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i['rank'], i['track_name'], i['length'], i['artist_name']) for i in dr]
+    cur.executemany("INSERT INTO songs (rank, track_name, length, artist_name) VALUES (?, ?, ?, ?);", to_db)
 
-    # create tables
-    if conn is not None:
-        # create artist table
-        create_table(conn, sql_create_artist_table, sqlite_insert_artist_query)
+    con.commit()
+    con.close()
 
-        # create song table
-        create_table(conn, sql_create_song_table, sqlite_insert_song_query)
-
-    else:
-        print("Error! cannot create the database connection.")
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -582,23 +497,6 @@ def create_connection(db_file):
         print(e)
 
     return conn
-
-
-def create_table(conn, create_table_sql, insert_table_data):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-        c.execute(insert_table_data)
-        conn.commit()
-
-
-    except Error as e:
-        print(e)
 
 
 """
